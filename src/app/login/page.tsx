c920a243d58d5e37eaa5dea72dc7ea8fcf786f78
Mateus Henrique <mateushbsb@gmail.com>
2023-10-16 16:15:16 -0300

    Feature: create login viewer

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss';
+
+export default function Login() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.loginContainer}>
+        <h2>Todo.List</h2>
+        <span>Usuário</span>
+        <input></input>
+        <span>Senha</span>
+        <input type="password"></input>
+        <button>Entrar</button>
+
+        <a href="">Criar Conta</a>
+      </div>
+    </section>
+  )
+}
