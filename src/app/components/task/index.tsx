12727dc45d40b955c8febee43d4fe122c478bec7
Mateus Henrique <mateushbsb@gmail.com>
2023-10-19 09:10:44 -0300

    Feature: TaskComponent

--- src/app/components/task/index.tsx
+++ src/app/components/task/index.tsx
@@ -19,12 +19,23 @@ export default function Task({ taskData }: TaskProps) {
 
   return (
     <section className={styles.container}>
-      <div>
-        <h3>Id# - {taskData.id}</h3>
+      <div className={styles.headerContainer}>
+        <h4>Id# - {taskData.id}</h4>
         <h4>18 - Agosto - 2023</h4>
       </div>
-      <div></div>
-      <div></div>
+      <div className={styles.bodyContainer}>
+        <h2>{taskData.title}</h2>
+        <p>{taskData.description}</p>
+      </div>
+      <div className={styles.footerContainer}>
+        <div className={styles.data}>
+          <p>Data de Inicio {taskData.startAt} - Data de Finalização {taskData.endAt}</p>
+        </div>
+        <div className={styles.btns}>
+          <button className={styles.btnDelete} >Remover</button>
+          <button className={styles.btnFinished} >Finalizar</button>
+        </div>
+      </div>
     </section>
   )
 }
